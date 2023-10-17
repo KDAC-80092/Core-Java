6c7d1c4164f8889d654071bd015a2ad5e032046c
74702Himanshu <himanshurathore2019@gmail.com>
2023-10-17 18:15:01 +0530

    Assignment6_1

---
+++ Assignment06/Asgn6_1/src/com/app/ExceptionLineTooLong.java
@@ -0,0 +1,17 @@
+package com.app;
+
+public class ExceptionLineTooLong extends RuntimeException {
+	private String message;
+	private int length;
+	public ExceptionLineTooLong(String message, int length) {
+		super(message);
+		this.message = message;
+		this.length = length;
+		
+		
+	}
+	@Override
+	public String toString() {
+		return "ExceptionLineTooLong [message=" + message + ", length=" + length + "]";
+	}
+}
